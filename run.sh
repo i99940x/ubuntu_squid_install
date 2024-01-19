afd515c49b5a871a8e3c4763616cc5b8fb437be2
i99940x <135556223+i99940x@users.noreply.github.com>
2024-01-19 14:49:55 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-wget -O ubuntu_squid_install.sh "https://github.com/i99940x/ubuntu_squid_install/blob/main/ubuntu_squid_install.sh"
+wget -O ubuntu_squid_install.sh "https://raw.githubusercontent.com/i99940x/ubuntu_squid_install/main/ubuntu_squid_install.sh"
 sudo bash ubuntu_squid_install.sh
