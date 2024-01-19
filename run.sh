8a84ba8d9d751401610a3bf5be040f2d1495a1bd
i99940x <135556223+i99940x@users.noreply.github.com>
2024-01-19 14:51:30 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
+# copy 2 dòng lệnh đưới đây và chạy trên ubuntu, squid sẽ mở cổng : 9999 làm proxy http
+
 wget -O ubuntu_squid_install.sh "https://raw.githubusercontent.com/i99940x/ubuntu_squid_install/main/ubuntu_squid_install.sh"
 sudo bash ubuntu_squid_install.sh
